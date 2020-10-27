9f1b3c6a5049bdc6c5b99e1e034b89ce2c5de3c0
Ayoola Paul <Ayopaul19@gmail.com>
2020-10-27 23:37:21 +0100

    for of method added

--- loops.js
+++ loops.js
@@ -2,6 +2,8 @@
  * Complete the vowelsAndConsonants function.
  * Print your output using 'console.log()'.
  */
+var s = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 function vowelsAndConsonants(s) {
 	let vowels = ["a", "e", "i", "o", "u"];
 	for (let i = 0; i < s.length; i++) {
@@ -16,3 +18,25 @@ function vowelsAndConsonants(s) {
 		}
 	}
 }
+
+
+
+// for of method
+
+function vowelsAndConsonants1(s) {
+	let vowels = ["a", "e", "i", "o", "u"];
+	for (const cur of s) {
+		if (vowels.indexOf(cur) > -1) {
+			console.log(cur);
+		}
+	}
+
+	for (const cur of s) {
+		if (vowels.indexOf(cur) < 0) {
+			console.log(cur);
+		}
+	}
+}
+
+
+vowelsAndConsonants1(s);
