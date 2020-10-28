88f2532a94de1a0e95f5641743359d0ad17fc715
Ayoola Paul <Ayopaul19@gmail.com>
2020-10-28 19:16:29 +0100

    for loop function name amended

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -5,17 +5,22 @@
  * nums: An array of numbers.
  */
 // map method
+
+console.time('map');
+
 function modifyArray(nums) {
-  return nums.map(n => n % 2 ? n * 3 : n * 2); 
+    return nums.map(n => n % 2 ? n * 3 : n * 2);
 }
+console.log(modifyArray([1, 2, 3, 4, 5]))
+console.timeEnd('map');
 
 // for loop method
-const modifyArray = (nums) => {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] % 2 === 0)
-      nums[i] *= 2;
-    else
-      nums[i] *= 3;
-  }
-  return nums;
+const modifyArrayX = (nums) => {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 === 0)
+            nums[i] *= 2;
+        else
+            nums[i] *= 3;
+    }
+    return nums;
 }
