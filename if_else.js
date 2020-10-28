14f3d9e446f38c880c4b4b951067c697b91370dd
Ayoola Paul <Ayopaul19@gmail.com>
2020-10-28 04:07:19 +0100

    switch method added if_else

--- if_else.js
+++ if_else.js
@@ -19,3 +19,31 @@ function getGrade(score) {
 
 
 // Switch method
+
+function getGrade2(score) {
+  let grade;
+
+   // write your code here
+ switch(true) {
+  case (score > 25 && score <= 30):
+    grade = 'A';
+    break;
+  case (score > 20 && score <= 25):
+    grade = 'B';
+    break;
+  case (score > 15 && score <= 20):
+    grade = 'C';
+    break;
+  case (score > 10 && score <= 15):
+    grade = 'D';
+    break;
+  case (score > 5 && score <= 10):
+    grade = 'E';
+    break;
+  default:
+    grade = 'F';
+  }
+return grade;
+
+}
+ 
