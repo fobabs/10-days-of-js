ede006789bd444dab744b8f44fb5a0312aa6009f
Ayoola Paul <Ayopaul19@gmail.com>
2020-10-28 03:37:07 +0100

    updated

--- loops.js
+++ loops.js
@@ -5,18 +5,18 @@
 var s = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 function vowelsAndConsonants(s) {
-	let vowels = ["a", "e", "i", "o", "u"];
-	for (let i = 0; i < s.length; i++) {
-		if (vowels.indexOf(s[i]) > -1) {
-			console.log(s[i]);
-		}
-	}
+    let vowels = ["a", "e", "i", "o", "u"];
+    for (let i = 0; i < s.length; i++) {
+        if (vowels.indexOf(s[i]) > -1) {
+            console.log(s[i]);
+        }
+    }
 
-	for (let j = 0; j < s.length; j++) {
-		if (vowels.indexOf(s[j]) < 0) {
-			console.log(s[j]);
-		}
-	}
+    for (let j = 0; j < s.length; j++) {
+        if (vowels.indexOf(s[j]) < 0) {
+            console.log(s[j]);
+        }
+    }
 }
 
 
@@ -24,19 +24,20 @@ function vowelsAndConsonants(s) {
 // for of method
 
 function vowelsAndConsonants1(s) {
-	let vowels = ["a", "e", "i", "o", "u"];
-	for (const cur of s) {
-		if (vowels.indexOf(cur) > -1) {
-			console.log(cur);
-		}
-	}
+    let vowels = ["a", "e", "i", "o", "u"];
+    for (const cur of s) {
+        if (vowels.indexOf(cur) > -1) {
+            console.log(cur);
+        }
+    }
 
-	for (const cur of s) {
-		if (vowels.indexOf(cur) < 0) {
-			console.log(cur);
-		}
-	}
+    for (const cur of s) {
+        if (vowels.indexOf(cur) < 0) {
+            console.log(cur);
+        }
+    }
 }
+
 
 
 vowelsAndConsonants1(s);
